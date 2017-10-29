8d7648876efd0c511632c073db1703fe1055e7b1
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-10-29 20:38:44 +0530

    Update

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ print('Python version ', platform.python_version())
 print('Vector Space Scoring')
 
 # Initializing all vaariables
-doc_1 = "Just keep using random words without any punctuation because that's what we need"
-doc_2 = "Another set of words which are added here just so that I can complete doc_2"
-doc_3 = "After doc_2 comes doc_3 what do you think we should do next after we're done typing random words"
+doc_1 = "Just keep using random words without any punctuation because because that's what we need need"
+doc_2 = "Another set of words which are added here just so that I can can complete doc_2"
+doc_3 = "After doc_2 comes doc_3 what do you think we should do next after we're done done typing random words"
 
 doc_1_list = doc_1.split(' ')
 doc_2_list = doc_2.split(' ')
@@ -54,18 +54,15 @@ for each in all_words:
 
 # Term Frequency calculation for document 1
 for each in all_words:
-	if each in doc_1_list:
-		doc_1_tf.update({each: doc_1_tf[each]+1})
+	doc_1_tf.update({each: doc_1_list.count(each)})
 	
 # Term Frequency calculation for document 2
 for each in all_words:
-	if each in doc_2_list:
-		doc_2_tf.update({each: doc_2_tf[each]+1})
-	
+	doc_2_tf.update({each: doc_2_list.count(each)})
+
 # Term Frequency calculation for document 3
 for each in all_words:
-	if each in doc_3_list:
-		doc_3_tf.update({each: doc_3_tf[each]+1})
+	doc_3_tf.update({each: doc_3_list.count(each)})
 
 # Document Frequency Calculation
 for each in all_words:
@@ -92,3 +89,19 @@ print('Inverted Document Frequency -')
 print(idf)
 
 # tf-idf calculation
+
+for each in all_words:
+	doc_1_tf_idf[each] = idf[each]*doc_1_tf[each];
+	doc_2_tf_idf[each] = idf[each]*doc_2_tf[each];
+	doc_3_tf_idf[each] = idf[each]*doc_3_tf[each];
+
+print()
+print('tf-idf value of Document 1 -')
+print(doc_1_tf_idf)
+print()
+print('tf-idf value of Document 2 -')
+print(doc_2_tf_idf)
+print()
+print('tf-idf value of Document 3 -')
+print(doc_3_tf_idf)
+print()
